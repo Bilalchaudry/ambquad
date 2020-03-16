1b53144bef4a4891c0c62333260eddb95e5842df
zeshan9128 <zeeshan.butt@novatoresols.com>
2020-03-16 21:34:47 +0500

    destroy

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
 
-  has_many :client_company_projects
-  has_many :client_companies, :through => :client_company_projects
+  has_many :client_company_projects, dependent: :destroy
+  has_many :client_companies, :through => :client_company_projects, dependent: :destroy
 
   has_many :user_projects
   has_many :users, :through => :user_projects
