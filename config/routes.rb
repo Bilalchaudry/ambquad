f074e89e2e1a7a2148f759ee12235d99913e1617
zeshan9128 <zeeshan.butt@novatoresols.com>
2020-03-16 20:09:19 +0500

    remove show route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :employee_types
   resources :projects
   resources :client_companies
-  resources :users, except: [:create]
+  resources :users, except: [:show]
   resources :plant_types
 end
