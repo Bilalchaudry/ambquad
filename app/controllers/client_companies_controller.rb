957b4a369b6391033f647f60c27f2c4e1f6cb33f
zeshan9128 <zeeshan.butt@novatoresols.com>
2020-03-16 20:46:06 +0500

    fix client company

--- app/controllers/client_companies_controller.rb
+++ app/controllers/client_companies_controller.rb
@@ -64,7 +64,7 @@ class ClientCompaniesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_company
-      @company = ClientCompany.find(params[:id])
+      @client_company = ClientCompany.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
