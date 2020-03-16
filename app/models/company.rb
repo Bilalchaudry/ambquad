9e142daa0bbc581c8312604d9244977fac19041e
Bila <bilal.novatore@gmail.com>
2020-03-16 15:22:23 +0500

    forms action and fixes

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
 
-  has_many :projects
+  has_many :projects, dependent: :destroy
   has_many :users
 
   validates :email, :name, :phone_number, :address, :number_of_users, :start_date, presence: true
