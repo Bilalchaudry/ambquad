eb696e1caafb7a65d53e7116ef10ff3ebb24eab5
zeshan9128 <zeeshan.butt@novatoresols.com>
2020-03-18 21:08:51 +0500

    adding default admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(email: 'admin@example.com', username: "admin", password: 'password')
+User.create(email: 'admin@ambquadtm.com', username: "admin", password: 'ambQuad2020!')
