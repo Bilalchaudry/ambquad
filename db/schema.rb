9107140b9729a2414d8b0e0e43530ed86e049155
zeshan9128 <zeeshan.butt@novatoresols.com>
2020-03-20 20:30:31 +0500

    show employees for project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_121155) do
+ActiveRecord::Schema.define(version: 2020_03_20_151420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -171,6 +171,7 @@ ActiveRecord::Schema.define(version: 2020_03_20_121155) do
     t.date "closed_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "employee_id"
   end
 
   create_table "temporary_users", force: :cascade do |t|
