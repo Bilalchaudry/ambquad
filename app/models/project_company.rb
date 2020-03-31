3a13479bf9f3dbeded70ca4f16c2644a3d69ec12
khurram <khurramrashid.novatore@gmail.com>
2020-03-31 11:08:11 +0500

    fix

--- app/models/project_company.rb
+++ app/models/project_company.rb
@@ -6,13 +6,18 @@ class ProjectCompany < ApplicationRecord
   has_many :project_and_project_companies
   has_many :projects, :through => :project_and_project_companies, dependent: :destroy
 
-  def self.import(file, user)
+  def self.import(file, user, project)
+    if user.client_company == nil
+      client_company = 1
+    else
+      client_company = user.client_company.id
+    end
     if File.extname(file.original_filename) == '.csv'
       file_name = file.original_filename
       CSV.foreach("public/documents/#{file_name}", headers: true) do |row|
-        employee = ProjectCompany.create(project_id: row[0], company_name: row[1], company_summary: row[2], project_role: row[3], address: row[4],
+        employee = ProjectCompany.create(company_name: row[1], company_summary: row[2], project_role: row[3], address: row[4],
                                          phone: row[5], primary_poc_first_name: row[6], primary_poc_last_name: row[7], poc_email: row[8],
-                                         poc_phone: row[9], client_company_id: user.client_company.id)
+                                         poc_phone: row[9], client_company_id: client_company, project_id: project.id)
       end
     else
       spreadsheet = open_spreadsheet(file)
