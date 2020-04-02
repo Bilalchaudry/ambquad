8c183846fcab5bd4b22db63271f0c8b265abfacc
khurram <khurramrashid.novatore@gmail.com>
2020-04-02 18:20:59 +0500

    inactive user fixed and import error fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,9 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(update_params)
+        if params[:temporary_user][:status] == "1"
+          @user.update(status: "Inactive")
+        end
         format.html {redirect_to @user, notice: 'User was successfully updated.'}
         format.json {render :show, status: :ok, location: @user}
       else
