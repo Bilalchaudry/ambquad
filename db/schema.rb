dcb1c4916f5a845293b58f4d59182da1b89a2bad
zeshan9128 <zeeshan.butt@novatoresols.com>
2020-04-16 13:42:22 +0500

    adding city state to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_190038) do
+ActiveRecord::Schema.define(version: 2020_04_16_084104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -317,6 +317,8 @@ ActiveRecord::Schema.define(version: 2020_04_15_190038) do
     t.string "confirm_token"
     t.string "user_id"
     t.integer "status"
+    t.string "city"
+    t.string "state"
   end
 
   create_table "time_sheet_cost_codes", force: :cascade do |t|
