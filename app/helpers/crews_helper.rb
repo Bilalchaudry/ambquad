8e6202355e255a8037b5d02f907e478e2aa7870d
khurram <khurramrashid.novatore@gmail.com>
2020-04-23 20:16:27 +0500

    celender fixed

--- app/helpers/crews_helper.rb
+++ app/helpers/crews_helper.rb
@@ -1,12 +1,12 @@
 module CrewsHelper
 
   def crew_plants
-    @crew_plants = @project.crews.get_all_plants
+    @crew_plants = @project.crews.get_all_plants(params[:id])
     @crews = @project.plants.reject { |plant| @crew_plants.pluck(:plant_id).include?(plant.id) }
   end
 
   def crew_employees
-    @crew_employee = @project.crews.get_all_employees
+    @crew_employee = @project.crews.get_all_employees(params[:id])
     @crews = @project.employees.reject { |employee| @crew_employee.pluck(:employee_id).include?(employee.id) }
   end
 
