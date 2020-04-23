636e429221a229c1a2af74d7c398d9b11e5067fc
Bila <bilal.novatore@gmail.com>
2020-04-23 13:34:53 +0500

    plant time sheet fix

--- app/helpers/plant_time_sheets_helper.rb
+++ app/helpers/plant_time_sheets_helper.rb
@@ -1,2 +1,12 @@
 module PlantTimeSheetsHelper
+  def checking_cost_code_and_total_hours(plant_time_sheet_id)
+    @cost_codes = @project.time_sheet_cost_codes.where(time_sheet_plant_id: plant_time_sheet_id)
+    @cost_codes_ar = @cost_codes.to_a
+    @cost_code_and_total_hours_sum = 0
+    for i in 0..4
+      if @cost_codes[i].present?
+        @cost_code_and_total_hours_sum += @cost_codes[i].hrs
+      end
+    end
+  end
 end
