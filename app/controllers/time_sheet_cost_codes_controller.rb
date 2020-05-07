d2e90e58089beb7a34ffbff20476beb154768b9c
Bila <bilal.novatore@gmail.com>
2020-05-07 13:19:21 +0500

    cost code fix

--- app/controllers/time_sheet_cost_codes_controller.rb
+++ app/controllers/time_sheet_cost_codes_controller.rb
@@ -68,7 +68,7 @@ class TimeSheetCostCodesController < ApplicationController
       @time_sheet_cost_code = @project.time_sheet_cost_codes.create(cost_code_id: params[:cost_code_id],
                                                                     cost_code: cost_codee,
                                                                     employee_id: params[:employee_id],
-                                                                    cost_code_created_at: params[:date],
+                                                                    cost_code_created_at: Date.today,
                                                                     time_sheet_employee_id: params[:time_sheet_employee_id],
                                                                     employee_time_sheet_id: params[:time_sheet_employee_id])
       @cost_code = @project.time_sheet_cost_codes.where(time_sheet_employee_id: params[:time_sheet_employee_id])
