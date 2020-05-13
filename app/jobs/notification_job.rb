5ad2c54949e2dbadbf9ce7bc6c1b00e85c6432a0
Bila <bilal.novatore@gmail.com>
2020-05-13 15:58:48 +0500

    after guides

--- app/jobs/notification_job.rb
+++
@@ -1,7 +0,0 @@
-class NotificationJob < ApplicationJob
-  queue_as :default
-
-  def perform(project_name,subject,project_user, user_email)
-    NotificationMailer.notification(project_name,subject,project_user,user_email).deliver_now
-  end
-end
--- config/initializers/schedular.rb
+++ config/initializers/schedular.rb
@@ -11,16 +11,15 @@ scheduler.every '1d' do
 end
 scheduler.cron '1,00 * * * *' do
   Project.all.each do |project|
-    @project_name = project.project_name
-    subject = @project_name + " " + "Data is incomplete."
     @project_employees_without_foreman = project.employees.where(foreman_id: nil)
     @project_plants_without_foreman = project.plants.where(foreman_id: nil)
     @project_cost_codes_without_budget_holder = project.cost_codes.where(budget_holder_id: nil)
-
     if @project_employees_without_foreman.present? || @project_plants_without_foreman.present? || @project_cost_codes_without_budget_holder.present?
+      @project_name = project.project_name
+      subject = @project_name + ' ' + ' data is incomplete.'
       project_users = project.client_company.users
       project_users.each do |project_user|
-        NotificationJob.perform_later (@project_name), (subject), (project_user.username), (project_user.email)
+        NotificationMailer.notification(@project_name,subject,project_user.username,project_user.email).deliver_now
       end
     end
   end
