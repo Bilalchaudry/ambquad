c98cc0c9e9b6d43d1e6aae80441f788b83c28278
Bila <bilal.novatore@gmail.com>
2020-05-13 13:36:17 +0500

    notification for admin and users

--- config/initializers/schedular.rb
+++ config/initializers/schedular.rb
@@ -76,24 +76,34 @@ require 'rufus-scheduler'
 # #
 # # end
 scheduler = Rufus::Scheduler.new
-scheduler.cron '21,13 * * * *' do
+scheduler.cron '1,12 * * * *' do
   Project.all.each do |project|
     @project_name = project.project_name
     @project_employees_without_foreman = project.employees.where(foreman_id: nil)
     @project_plants_without_foreman = project.plants.where(foreman_id: nil)
-    # if @project_employees_without_foreman.present?
-    #   project_users = project.client_company.users
-    #   message = "There are employees without foreman."
-    #   project_users.each do | project_user|
-    #   NotificationJob.perform_later (@project_name),(message),(project_user.username),(project_user.email)
-    #   end
-    # end
+    @project_cost_codes_without_budget_holder = project.cost_codes.where(budget_holder_id: nil)
+
+    if @project_employees_without_foreman.present?
+      project_users = project.client_company.users
+      message = "There are employees without foreman."
+      project_users.each do |project_user|
+        NotificationJob.perform_later (@project_name), (message), (project_user.username), (project_user.email)
+      end
+    end
 
     if @project_plants_without_foreman.present?
       project_users = project.client_company.users
       message = "There are plants without foreman."
-      project_users.each do | project_user|
-        NotificationJob.perform_later (@project_name),(message),(project_user.username),(project_user.email)
+      project_users.each do |project_user|
+        NotificationJob.perform_later (@project_name), (message), (project_user.username), (project_user.email)
+      end
+    end
+
+    if @project_cost_codes_without_budget_holder.present?
+      project_users = project.client_company.users
+      message = "There are cost codes without budget holder."
+      project_users.each do |project_user|
+        NotificationJob.perform_later (@project_name), (message), (project_user.username), (project_user.email)
       end
     end
   end
